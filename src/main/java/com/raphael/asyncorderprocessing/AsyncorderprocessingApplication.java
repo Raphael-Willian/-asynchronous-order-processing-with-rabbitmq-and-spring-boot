20db2b1be96b8b12c54fad9dbdd543acda01e108
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-04-14 09:58:44 -0300

    initial commit of application

---
+++ src/main/java/com/raphael/asyncorderprocessing/AsyncorderprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.asyncorderprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncorderprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncorderprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphael/asyncorderprocessing/AsyncorderprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.asyncorderprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncorderprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

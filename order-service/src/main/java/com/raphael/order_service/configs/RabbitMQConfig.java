72e24b3e08755e226c4a11767e44a35eef4c7b15
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-04-14 23:19:16 -0300

    feat: created RabbitMQConfig and implemented beans

---
+++ order-service/src/main/java/com/raphael/order_service/configs/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.raphael.order_service.configs;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE = "order.queue";
+
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+     return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true); //Boolean for definition durable of queue
+    }
+
+
+}

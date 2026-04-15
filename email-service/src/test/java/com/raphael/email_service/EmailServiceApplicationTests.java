918bee5b79c38728215b209465cce1f250b15eb4
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-04-14 22:17:04 -0300

    initial commit in application

---
+++ email-service/src/test/java/com/raphael/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
